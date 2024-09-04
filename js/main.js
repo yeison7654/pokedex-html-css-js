01d8dc6f33c58b89281d520193ba6bbdd580fd47
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-09-04 12:00:27 -0500

    pokedex finalizada v1.00

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let idPokemon = 0;
 let views = document.querySelectorAll(".height-23");
-let form = document.querySelector("#form-consult")
+let form = document.querySelector("#form-consult");
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         formConsult();
@@ -8,13 +8,22 @@ document.addEventListener("DOMContentLoaded", () => {
         btnControls();
     }, 1000);
 })
+document.addEventListener("click", () => {
 
+})
 function formConsult() {
     form.addEventListener("submit", (e) => {
         e.preventDefault()
         let txtName = e.target.input_name.value;
         if (txtName == "") {
-            alert("Campos vacio");
+            views[3].classList.toggle("hidden")
+            views[0].classList.toggle("hidden")
+            document.querySelector(".title-alert").innerHTML = "OCURRIO UN ERROR INESPERADO";
+            document.querySelector(".text-alert").innerHTML = "El campo de busqueda no puede ser enviado como vacio";
+            setTimeout(() => {
+                views[3].classList.toggle("hidden")
+                views[0].classList.toggle("hidden")
+            }, 1500);
             return false;
         }
         searchPokemon(txtName);
@@ -24,6 +33,7 @@ function onOff() {
     let btn_power = document.querySelector(".btn-power");
     btn_power.addEventListener("click", () => {
         if (views[0].classList.contains("hidden")) {
+            let btn_gray = document.querySelectorAll(".btn-gray");
             //accion prender
             document.querySelector(".input-name").disabled = false;
             viewsHidden();
@@ -32,9 +42,19 @@ function onOff() {
                 views[0].classList.toggle("hidden");
                 views[2].classList.toggle("hidden");
             }, 1500);
-            document.querySelector(".text-load").innerHTML = "Iniciado equipo";
-           // searchPokemon(nRandom());
+            document.querySelector(".text-load").innerHTML = "Iniciando equipo";
+            /*cambio de clases*/
+            btn_gray[0].classList.replace("btn-gray", "btn-controls");
+            btn_gray[1].classList.replace("btn-gray", "btn-controls");
+            btn_gray[2].classList.replace("btn-gray", "btn-search");
+            /*habilitar btn */
+            btn_gray[0].disabled = false;
+            btn_gray[1].disabled = false;
+            btn_gray[2].disabled = false;
+            searchPokemon(nRandom(), 0);
         } else {
+            let btn_controls = document.querySelectorAll(".btn-controls");
+            let btn_search = document.querySelector(".btn-search");
             //accion apagar
             form.reset();
             document.querySelector(".input-name").disabled = true;
@@ -45,6 +65,14 @@ function onOff() {
                 views[2].classList.toggle("hidden");
             }, 1500);
             document.querySelector(".text-load").innerHTML = "Apagando equipo";
+            /*cambio de clases */
+            btn_controls[0].classList.replace("btn-controls", "btn-gray");
+            btn_controls[1].classList.replace("btn-controls", "btn-gray");
+            btn_search.classList.replace("btn-search", "btn-gray");
+            /*btn desactivar */
+            btn_controls[0].disabled = true;
+            btn_controls[1].disabled = true;
+            btn_search.disabled = true;
         }
 
     })
@@ -77,13 +105,15 @@ function idIncrement(params) {
     return idPokemon
 }
 
-function searchPokemon(params) {
+function searchPokemon(params, status = 1) {
     let Url = "https://pokeapi.co/api/v2/pokemon/";
     try {
         let urlEnd = Url + params;
-        views[2].classList.toggle("hidden")
-        document.querySelector(".text-load").innerHTML = "Realizando la busqueda";
-        views[0].classList.toggle("hidden")
+        if (status == 1) {
+            views[2].classList.toggle("hidden")
+            document.querySelector(".text-load").innerHTML = "Realizando la busqueda";
+            views[0].classList.toggle("hidden")
+        }
         fetch(urlEnd)
             .then((response) => response.json())
             .then(data => {
@@ -95,10 +125,12 @@ function searchPokemon(params) {
                 document.querySelector(".sound-pokemon").src = data.cries.latest
                 document.querySelector(".base_experience").innerHTML = data.base_experience
                 document.querySelector(".weight").innerHTML = data.weight + " Kg"
-                setTimeout(() => {
-                    views[2].classList.toggle("hidden")
-                    views[0].classList.toggle("hidden")
-                }, 500);
+                if (status == 1) {
+                    setTimeout(() => {
+                        views[2].classList.toggle("hidden")
+                        views[0].classList.toggle("hidden")
+                    }, 500);
+                }
             })
     } catch (error) {
         console.log("Se encontro un error inesperado : " + error)
